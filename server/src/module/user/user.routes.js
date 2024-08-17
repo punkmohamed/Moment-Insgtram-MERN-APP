cdda047b7e0f0581c8469d21b3d8283bb6dad2e1
punkmohamed <punkmohamed22@gmail.com>
2024-08-18 00:32:03 +0200

    work/

--- server/src/module/user/user.routes.js
+++ server/src/module/user/user.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
 const router = express.Router();
 
-import { signin, signup } from "./user.controllers.js";
+import { getUser, signin, signup } from "./user.controllers.js";
 
 router.post("/signin", signin);
 router.post("/signup", signup);
+router.get("/", getUser);
 
 export default router;
